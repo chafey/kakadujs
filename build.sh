93f23a44009716066db6ef4d1118b023778c1d72
Chris Hafey <cehafey@amazon.com>
2024-03-07 12:02:03 -0600

    cleaned up build script

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@
 # this script does a clean build and is useful for development when modifying cmakefiles
 clear
 rm -rf build
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release 
-#(cd build && make VERBOSE=1 -j) || { exit 1; }
-(cd build && make -j) || { exit 1; }
+
+export CMAKE_BUILD_TYPE=Release # | Debug
+#export VERBOSE=1
+
+(cmake -S . -B build -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE) || (exit 1;)
+(cmake --build build --  -j) || (exit 1;)
 build/test/cpp/cpptest
